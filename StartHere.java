b76b31b298d92d10b525851e5aa233689878af10
Ammaar <fakirammaar8@gmail.com>
2021-04-01 14:04:39 +0200

    created a class

---
+++ StartHere.java
@@ -0,0 +1,10 @@
+public class StartHere {
+
+    public static void main(String[] args) {
+        if (args.length>0) {
+            System.out.println(args[0] + " is starting with Java.");
+        } else {
+System.out.println("I am starting with Java.");
+        }
+    }
+}
